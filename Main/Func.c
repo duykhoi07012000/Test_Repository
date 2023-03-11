198a143714cc2007659c1d9af08b3b721113238a
Le Nguyen Duy Khoi <lenguyenduykhoi10@gmail.com>
2023-03-11 16:46:57 +0700

    Khoi modify file Func.c

--- Main/Func.c
+++ Main/Func.c
@@ -1,112 +1,71 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#define N 200001
-char a[N];
+#include <stdlib.h>
+int *Merge(int *a, int n1, int *b, int n2)
+{
+    int *c = malloc(sizeof(a)+sizeof(b));
+    int i, j, cnt = 0;
+    while(i < n1 && j < n2)
+    {
+        if(a[i] < b[j])
+        {
+            c[cnt++] = a[i++];
+        }
+        else
+        {
+            c[cnt++] = b[j++];
+        }
+    }
+    for(int k = i; k < n1; k++)
+    {
+        c[cnt++] = a[k];
+    }
+    for(int kk = j; kk < n2; kk++)
+    {
+        c[cnt++] = b[kk];
+    }
+    return c;
+}
 
-// int Calculate(char a[])
-// {
-//     int len = strlen(a);
-//     int cnt = 0;
-//     bool fCheck = true;
-//     bool fStop = true;
-//     int i = 0;
-//     int ii = 0;
-//     int cnt_b = 0;
-//     while(fCheck == true)
-//     {
-//         if(a[i] == 'B')
-//         {
-//             cnt_b++;
-//             if(a[i+1] == 'W')
-//             {
-//                 *&a[i+1] = 'B';
-//                 i = cnt_b+ii;
-//                 cnt += cnt_b;
-//                 cnt_b = 0;
-//             }
-//             else
-//             {
-//                 if(i == len-1 && a[i] != 'W')
-//                 {
-//                     fCheck = false;
-//                 }
-//                 else
-//                 {
-//                     i++;
-//                 }
-//             }
-//         }
-//         if(a[ii] == 'W' && fStop == true)
-//         {
-//             ii++;
-//             if(a[ii] == 'B')
-//             {
-//                 i = ii;
-//                 fStop = false;
-//             }
-//             else if(ii == len-1 && a[ii] == 'W')
-//             {
-//                 fCheck = false;
-//             }
-//         }
-//     }
-//     return cnt;
-// }
+int *MergeSort(int a[], int n)
+{
+    if(n == 1)
+    {
+        return a;
+    }
+    int mid = n/2;
+    int *a1 = malloc(sizeof(mid));
+    int *a2 = malloc(sizeof(n-mid));
+    for(int i = 0; i < mid; i++)
+    {
+        a1[i] = a[i];
+    }
+    for(int j = 0; j < n-mid; j++)
+    {
+        a2[j] = a[mid + j];
+    }
+    a1 = MergeSort(a1, mid);
+    free(a1);
+    a2 = MergeSort(a2, n-mid);
+    free(a2);
+    return Merge(a1, mid, a2, n-mid);
+}
 
 int main(void)
 {
-    int T, TestCase, re;
-    scanf("%d", &T);
-    for(TestCase = 1; TestCase <= T; ++TestCase)
+    // int b[] = {2, 5, 6};
+    // int a[] = { 3, 8, 9, 10, 15, 20};
+    // int *c = Merge(a, 6, b, 3);
+    // for(int i = 0; i < 9; i++)
+    // {
+    //     printf("%d\t", c[i]);
+    // }
+    // free(c);
+    int a[] = {15, 45, 1, 3, 5, 9, 8, 9};
+    int n = sizeof(a)/sizeof(a[0]);
+    int *b = MergeSort(a, n);
+    for(int i = 0; i < n; i++)
     {
-        scanf("%s", &a);
-        int len = strlen(a);
-        int cnt = 0;
-        bool fCheck = true;
-        bool fStop = true;
-        int i = 0;
-        int ii = 0;
-        int cnt_b = 0;
-        while(fCheck == true)
-        {
-            if(a[i] == 'B')
-            {
-                cnt_b++;
-                if(a[i+1] == 'W')
-                {
-                    *&a[i+1] = 'B';
-                    i = cnt_b+ii;
-                    cnt += cnt_b;
-                    cnt_b = 0;
-                }
-                else
-                {
-                    if(i == len-1 && a[i] != 'W')
-                    {
-                        fCheck = false;
-                    }
-                    else
-                    {
-                        i++;
-                    }
-                }
-            }
-            if(a[ii] == 'W' && fStop == true)
-            {
-                ii++;
-                if(a[ii] == 'B')
-                {
-                    i = ii;
-                    fStop = false;
-                }
-                else if(ii == len-1 && a[ii] == 'W')
-                {
-                    fCheck = false;
-                }
-            }
-        }
-        printf("#%d %d\n", TestCase, cnt);
+        printf("%d\t", b[i]);
     }
     return 0;
 }
