360576a401722b3bf455e2ee002e6d19f3af1696
Le Nguyen Duy Khoi <lenguyenduykhoi10@gmail.com>
2023-03-11 16:49:21 +0700

    Khoi recently have modified file Lib.c

--- Lib/Lib.c
+++ Lib/Lib.c
@@ -1,85 +1,61 @@
 #include <stdio.h>
 #include <stdbool.h>
-#define MAX 100
-void NhapDSDac(int a[], int *n)
-{
-    printf("Nhap n: ");
-    scanf("%d", n);
-    for(int i = 0; i < *n; i++)
-    {
-        printf("Nhap a[%d] = ", i);
-        scanf("%d", &a[i]);
-    }
-}
-
-void XuatDSDac(const int a[], const int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        printf("a[%d] = %d \t", i, a[i]);
-    }
-    printf("\n");
-} 
-
-int TimDauTien(int a[], int n, int x)
-{
-    int vt = -1;
-    for(int i = 0; i < n; i++)
-    {
-        if(a[i] == x)
-        {
-            vt = i;
-            break;
-        }
-    }
-    return vt;
-}
-
-bool ChenViTri(int a[], int *n, int vitri, int x)
-{
-    if(vitri < 0 || vitri > *n)
-    {
-        return false;
-    }
-    else
-    {
-        (*n)++;
-        for(int i = (*n); i >= vitri; i--)
-        {
-            a[i] = a[i-1];
-        }
-        a[vitri] = x;
-        return true;
-    }
-}
-
+#define N 100000
 int main(void)
 {
-    int a[MAX];
-    int n;
-    int x;
-    NhapDSDac(a, &n);
-    XuatDSDac(a, n);
-    printf("Nhap x = ");
-    scanf("%d", &x);
-    int vitri;
-    printf("Nhap vi tri muon chen = ");
-    scanf("%d", &vitri);
-    printf("====Ket qua chen===");
-    if(ChenViTri(a,&n,vitri,x) == true)
+    int a[N];
+    int T, TestCase, NumSize, cnt, check;
+    scanf("%d", &T);
+    for(TestCase = 1; TestCase <= T; ++TestCase)
     {
-        XuatDSDac(a,n);
+        scanf("%d", &NumSize);
+        for(int i = 0; i < NumSize; i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        cnt = 0;
+        int Pos = 0;
+        int Check = 0;
+        for(int j = 0; j == Check; j++)
+        {
+            for(int k = Pos; k < NumSize-2; k++)
+            {
+                if(a[k] < a[k+1])
+                {
+                    if(a[k+1] <= a[k+2])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Pos = k+2;
+                        Check++;
+                        cnt++;
+                        break;
+                    }
+                }
+                else if(a[k] > a[k+1])
+                {
+                    if(a[k+1] >= a[k+2])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Pos = k+2;
+                        Check++;
+                        cnt++;
+                        break;
+                    }
+                }
+                else
+                {
+                    continue;
+                }                
+            }
+        }    
+        printf("%d\n", cnt+1);
     }
-    // printf("Nhap x = ");
-    // scanf("%d", &x);
-    // int vt = TimDauTien(a, n, x);
-    // if(vt == -1)
-    // {
-    //    printf("Khong tim thay"); 
-    // }
-    // else
-    // {
-    //     printf("Vi tri la %d", vt);
-    // }
+    
     return 0;
 }
